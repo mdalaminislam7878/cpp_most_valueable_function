4f02ecafa7f33be4365c13f54c0728c1649ca612
mdalaminislam7878 <51321697+mdalaminislam7878@users.noreply.github.com>
2020-08-22 22:24:59 +0600

    Create binary_search.cpp

---
+++ sorting/binary_search.cpp
@@ -0,0 +1,17 @@
+int binary_search(int A[],int n,int x){
+  int mid,i,left,right;
+  left=0;
+  right=n-1;
+  while(left<=right){
+    mid=(left+right)/2;
+    if(A[mid]==x){
+      	return mid;
+    }
+	if(A[mid]<x){
+		left=mid+1;
+	}else{
+		right=mid-1;
+	}
+  }
+  return -1;
+}
